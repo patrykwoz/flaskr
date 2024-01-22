c51e76d8fe9e46d0aafda3b1eb5bc375c25b2749
root <root@PATRYK-NFHTHTE0>
2024-01-21 21:24:03 -0500

    Celery DB

--- flaskr/app.py
+++ flaskr/app.py
@@ -1,6 +1,8 @@
 from flask import Flask, request, render_template
 from celery import Celery
 
+from flaskr.models import db, KnowledgeBase
+
 
 app = Flask(__name__)
 celery = Celery(app.name, broker='redis://localhost:6379/0')
@@ -18,3 +20,23 @@ def start_ml_task():
         return f"Task ID: {task.id}"
 
     return render_template('add-test.html')
+
+@app.route("/kbs/add")
+def render_add_kb():
+    
+    return render_template('add-kb.html')
+
+@app.route("/kbs/add")
+def add_kb() -> dict[str, object]:
+
+    title = request.form.get("title")
+
+    new_kb = KnowledgeBase(title=title)
+    db.session.add(new_kb)
+    db.session.commit()
+    kb_id = new_kb.id
+
+
+    result = celery.send_task('celery_worker.create_kb', kwargs={kb_id})
+
+    return {"result_id": result.id}
